b3ef05cf08beb9f6e3f6ac0ce0d688028aeed696
Mikhail Campos Guadamuz <plageat90@gmail.com>
2021-11-01 14:15:36 +0300

    updating to match a new dep for qgrapher

--- install_qrandom.sh
+++ install_qrandom.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-DEB_QINSTALLER="https://github.com/AlexandrDedckov/qinstaller/raw/master/qinstaller_1.1-1.deb"
-DEB_QUANTRA="https://github.com/AlexandrDedckov/qinstaller/raw/master/python3-quantra_0.1.2-1_all.deb"
-DEB_QUANTRALIB="https://github.com/AlexandrDedckov/qinstaller/raw/master/python3-quantralib_2.1.3-1_all.deb"
+DEB_QINSTALLER="https://github.com/quantum-sp8de/quantra-installer/raw/master/qinstaller_1.1-1.deb"
+DEB_QUANTRA="https://github.com/quantum-sp8de/quantra-installer/raw/master/python3-quantra_0.1.2-1_all.deb"
+DEB_QUANTRALIB="https://github.com/quantum-sp8de/quantra-installer/raw/master/python3-quantralib_2.1.3-1_all.deb"
 DEB_COLANDER="http://security.ubuntu.com/ubuntu/pool/universe/p/python-colander/python3-colander_1.0b1-3_all.deb"
 DEB_BASE58="http://security.ubuntu.com/ubuntu/pool/universe/p/python-base58/python3-base58_1.0.3-1_all.deb"
 EOS_DEB_UBUNTU_16_04="https://github.com/eosio/eos/releases/download/v2.0.12/eosio_2.0.12-1-ubuntu-16.04_amd64.deb"
 EOS_DEB_UBUNTU_18_04="https://github.com/eosio/eos/releases/download/v2.0.12/eosio_2.0.12-1-ubuntu-18.04_amd64.deb"
+DEB_QGRAPHER_18_04=""
+DEB_QGRAPHER_20_04=""
 
 usage() { echo "Usage: $0 [-r <validator|generator|user>]" 1>&2; exit 1; }
 
@@ -37,27 +39,23 @@ while getopts ":r:" o; do
             if [ "$r" == "validator" ]; then
                 TO_CHECK="qinstaller"
                 if [ "$VERSION_CODENAME" == "xenial" ]; then
-                    PKGS="$EOS_DEB_UBUNTU_16_04 $DEB_PKG $DEB_QINSTALLER"
+                    PKGS="$EOS_DEB_UBUNTU_16_04 $DEB_QINSTALLER"
                 elif [ "$VERSION_CODENAME" == "bionic" ]; then
-                    PKGS="$EOS_DEB_UBUNTU_18_04 $DEB_PKG $DEB_QINSTALLER"
+                    PKGS="$EOS_DEB_UBUNTU_18_04 $DEB_QINSTALLER"
                 fi
             elif [ "$r" == "generator" ]; then
                 TO_CHECK="quantra"
                 if [ "$VERSION_CODENAME" == "bionic" ]; then
-                    PKGS="$DEB_QUANTRA $DEB_QUANTRALIB $DEB_COLANDER $DEB_BASE58"
-                elif [ "$VERSION_CODENAME" == "focal" ] || \
-                     [ "$VERSION_CODENAME" == "groovy" ] || \
-                     [ "$VERSION_CODENAME" == "hirsute" ]; then
-                    PKGS="$DEB_QUANTRA $DEB_QUANTRALIB $DEB_COLANDER"
+                    PKGS="$DEB_QUANTRA $DEB_QUANTRALIB $DEB_QGRAPHER_18_04 $DEB_COLANDER $DEB_BASE58"
+                elif [ "$VERSION_CODENAME" == "focal" ]; then
+                    PKGS="$DEB_QUANTRA $DEB_QUANTRALIB $DEB_QGRAPHER_20_04 $DEB_COLANDER"
                 fi
             elif [ "$r" == "user" ]; then
                 TO_CHECK="quantrapy"
                 if [ "$VERSION_CODENAME" == "bionic" ]; then
-                    PKGS="$DEB_QUANTRALIB $DEB_COLANDER $DEB_BASE58"
-                elif [ "$VERSION_CODENAME" == "focal" ] || \
-                     [ "$VERSION_CODENAME" == "groovy" ] || \
-                     [ "$VERSION_CODENAME" == "hirsute" ]; then
-                    PKGS="$DEB_QUANTRALIB $DEB_COLANDER"
+                    PKGS="$DEB_QUANTRALIB $DEB_QGRAPHER_18_04 $DEB_COLANDER $DEB_BASE58"
+                elif [ "$VERSION_CODENAME" == "focal" ] ||; then
+                    PKGS="$DEB_QUANTRALIB $DEB_QGRAPHER_20_04 $DEB_COLANDER"
                 fi
             else
                 usage
