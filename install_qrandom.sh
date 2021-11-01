d836f1e427d9f062e79a9fde9ba183625c58f544
Mikhail Campos Guadamuz <plageat90@gmail.com>
2021-11-01 15:49:21 +0300

    updating to new version of deps and components

--- install_qrandom.sh
+++ install_qrandom.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+#
+# The main quantum-sp8de installation script
+#
+
 DEB_QINSTALLER="https://github.com/quantum-sp8de/quantra-installer/raw/master/qinstaller_1.1-1.deb"
-DEB_QUANTRA="https://github.com/quantum-sp8de/quantra-installer/raw/master/python3-quantra_0.1.2-1_all.deb"
-DEB_QUANTRALIB="https://github.com/quantum-sp8de/quantra-installer/raw/master/python3-quantralib_2.1.3-1_all.deb"
+DEB_QUANTRA="https://github.com/quantum-sp8de/quantra-installer/raw/master/python3-quantra_0.2.1-1_all.deb"
+DEB_QUANTRALIB="https://github.com/quantum-sp8de/quantra-installer/raw/master/python3-quantralib_2.2.1-1_all.deb"
 DEB_COLANDER="http://security.ubuntu.com/ubuntu/pool/universe/p/python-colander/python3-colander_1.0b1-3_all.deb"
 DEB_BASE58="http://security.ubuntu.com/ubuntu/pool/universe/p/python-base58/python3-base58_1.0.3-1_all.deb"
 EOS_DEB_UBUNTU_16_04="https://github.com/eosio/eos/releases/download/v2.0.12/eosio_2.0.12-1-ubuntu-16.04_amd64.deb"
 EOS_DEB_UBUNTU_18_04="https://github.com/eosio/eos/releases/download/v2.0.12/eosio_2.0.12-1-ubuntu-18.04_amd64.deb"
-DEB_QGRAPHER_18_04=""
-DEB_QGRAPHER_20_04=""
+DEB_QGRAPHER_18_04="https://github.com/quantum-sp8de/quantra-installer/raw/master/python3-qcrypgrapher_1.0-1.deb"
+DEB_QGRAPHER_20_04="https://github.com/quantum-sp8de/quantra-installer/raw/master/python3-qcrypgrapher_2.0-1.deb"
 
 usage() { echo "Usage: $0 [-r <validator|generator|user>]" 1>&2; exit 1; }
 
@@ -54,7 +58,7 @@ while getopts ":r:" o; do
                 TO_CHECK="quantrapy"
                 if [ "$VERSION_CODENAME" == "bionic" ]; then
                     PKGS="$DEB_QUANTRALIB $DEB_QGRAPHER_18_04 $DEB_COLANDER $DEB_BASE58"
-                elif [ "$VERSION_CODENAME" == "focal" ] ||; then
+                elif [ "$VERSION_CODENAME" == "focal" ]; then
                     PKGS="$DEB_QUANTRALIB $DEB_QGRAPHER_20_04 $DEB_COLANDER"
                 fi
             else
