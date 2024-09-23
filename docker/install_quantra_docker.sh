7c0b8e4abde75cd926ad6acefbb6176f63e18e5f
Mikhail Campos Guadamuz <Mikhail_Campos-Guadamuz@epam.com>
2024-09-23 12:19:29 +0300

    no need in qcicada .so file, it is integrated in quantra now as python code

--- docker/install_quantra_docker.sh
+++ docker/install_quantra_docker.sh
@@ -6,18 +6,11 @@
 
 
 IMG_NAME="docker_quantra"
-QCICADA_LIB="/opt/qrng/cicada-pkcs11.so"
-
 
 if ! command -v docker -v &> /dev/null
 then
     echo "'docker' command is not found. Please install docker or podman first"
     exit 1
-fi
-
-if ! [[ -f $QCICADA_LIB ]]; then
-  echo "QCicada library is not found. Please, install QCicada hardware first"
-  exit 3
 fi
 
 cat << EOF | docker build --no-cache -t $IMG_NAME -
@@ -27,7 +20,6 @@ RUN apt-get update -y && \
     apt-get install -y curl wget
 
 RUN curl -s https://raw.githubusercontent.com/quantum-sp8de/quantra-installer/master/install_qrandom.sh | bash -s -- -r generator
-RUN mkdir -p /opt/qrng && chmod 777 /opt/qrng/
 
 WORKDIR /tmp
 EOF
