90c74b717bdac2b3c61c8f4b41f4424ca4663e4d
Mikhail Campos Guadamuz <Mikhail_Campos-Guadamuz@epam.com>
2024-11-21 14:54:59 +0300

    quantralib version bump

--- install_qrandom.sh
+++ install_qrandom.sh
@@ -6,7 +6,7 @@
 
 DEB_QINSTALLER="https://github.com/quantum-sp8de/quantra-installer/raw/master/qinstaller_1.2-1.deb"
 DEB_QUANTRA="https://github.com/quantum-sp8de/quantra-installer/raw/master/python3-quantra_0.3.18-1_all.deb"
-DEB_QUANTRALIB="https://github.com/quantum-sp8de/quantra-installer/raw/master/python3-quantralib_2.3.4-1_all.deb"
+DEB_QUANTRALIB="https://github.com/quantum-sp8de/quantra-installer/raw/master/python3-quantralib_2.3.5-1_all.deb"
 DEB_COLANDER="http://security.ubuntu.com/ubuntu/pool/universe/p/python-colander/python3-colander_1.0b1-3_all.deb"
 DEB_BASE58="http://security.ubuntu.com/ubuntu/pool/universe/p/python-base58/python3-base58_1.0.3-1_all.deb"
 EOS_DEB_UBUNTU_18_04="https://github.com/EOSIO/eos/releases/download/v2.1.0/eosio_2.1.0-1-ubuntu-18.04_amd64.deb"
