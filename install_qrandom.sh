b4fd6d9a613b8445c2623c573b5e39208e763da6
Mikhail Campos Guadamuz <plageat90@gmail.com>
2024-11-24 11:05:39 +0000

    Support of armv7l arch for generator and user roles

--- install_qrandom.sh
+++ install_qrandom.sh
@@ -4,6 +4,8 @@
 # The main quantum-sp8de installation script
 #
 
+ARCH=$(arch)
+
 DEB_QINSTALLER="https://github.com/quantum-sp8de/quantra-installer/raw/master/qinstaller_1.2-1.deb"
 DEB_QUANTRA="https://github.com/quantum-sp8de/quantra-installer/raw/master/python3-quantra_0.3.18-1_all.deb"
 DEB_QUANTRALIB="https://github.com/quantum-sp8de/quantra-installer/raw/master/python3-quantralib_2.3.5-1_all.deb"
@@ -13,7 +15,14 @@ EOS_DEB_UBUNTU_18_04="https://github.com/EOSIO/eos/releases/download/v2.1.0/eosi
 EOS_DEB_UBUNTU_20_04="https://github.com/EOSIO/eos/releases/download/v2.1.0/eosio_2.1.0-1-ubuntu-20.04_amd64.deb"
 DEB_QGRAPHER_18_04="https://github.com/quantum-sp8de/quantra-installer/raw/master/python3-qcrypgrapher_1.0-1.deb"
 DEB_QGRAPHER_20_04="https://github.com/quantum-sp8de/quantra-installer/raw/master/python3-qcrypgrapher_2.0-1.deb"
-DEB_QGRAPHER_22_04="https://github.com/quantum-sp8de/quantra-installer/raw/master/python3-qcrypgrapher_3.0-1.deb"
+if [ "$ARCH" == "x86_64" ]; then
+  DEB_QGRAPHER_22_04="https://github.com/quantum-sp8de/quantra-installer/raw/master/python3-qcrypgrapher_3.0-1.deb"
+elif [ "$ARCH" == "armv7l" ]; then
+  DEB_QGRAPHER_22_04="https://github.com/quantum-sp8de/quantra-installer/raw/master/python3-qcrypgrapher_3.1-1_armhf.deb"
+else
+  echo "Not supported arch: $ARCH"
+  exit 1
+fi
 DEB_CHIP_CONVERTER_UBUNTU="https://github.com/quantum-sp8de/quantra-installer/raw/master/qrng-chip-converter_1.1-1.deb"
 
 DEB_OPENSSL_1_1_FOR_UBUNTU_22_04="http://security.ubuntu.com/ubuntu/pool/main/o/openssl/openssl_1.1.1f-1ubuntu2.23_amd64.deb"
@@ -47,6 +56,7 @@ while getopts ":r:" o; do
         r)
             r=${OPTARG}
             if [ "$r" == "validator" ]; then
+                [ "$ARCH" != "x86_64" ] && { echo "Not supported arch: $ARCH"; exit 1;}
                 TO_CHECK="qinstaller"
                 if [ "$VERSION_CODENAME" == "bionic" ]; then
                     PKGS="$EOS_DEB_UBUNTU_18_04 $DEB_QINSTALLER"
@@ -58,17 +68,28 @@ while getopts ":r:" o; do
             elif [ "$r" == "generator" ]; then
                 TO_CHECK="quantra"
                 if [ "$VERSION_CODENAME" == "focal" ]; then
+                    [ "$ARCH" != "x86_64" ] && { echo "Not supported arch: $ARCH"; exit 1;}
                     PKGS="$DEB_QUANTRA $DEB_QUANTRALIB $DEB_QGRAPHER_20_04 $DEB_CHIP_CONVERTER_UBUNTU $DEB_COLANDER"
                 elif [ "$VERSION_CODENAME" == "jammy" ]; then
+                    if [ "$ARCH" != "x86_64" ] && [ "$ARCH" != "armv7l" ]; then
+                        echo "Not supported arch: $ARCH"
+                        exit 1
+                    fi
                     PKGS="$DEB_QUANTRA $DEB_QUANTRALIB $DEB_QGRAPHER_22_04 $DEB_CHIP_CONVERTER_UBUNTU $DEB_COLANDER"
                 fi
             elif [ "$r" == "user" ]; then
                 TO_CHECK="quantrapy"
                 if [ "$VERSION_CODENAME" == "bionic" ]; then
+                    [ "$ARCH" != "x86_64" ] && { echo "Not supported arch: $ARCH"; exit 1;}
                     PKGS="$DEB_QUANTRALIB $DEB_QGRAPHER_18_04 $DEB_CHIP_CONVERTER_UBUNTU $DEB_COLANDER $DEB_BASE58"
                 elif [ "$VERSION_CODENAME" == "focal" ]; then
+                    [ "$ARCH" != "x86_64" ] && { echo "Not supported arch: $ARCH"; exit 1;}
                     PKGS="$DEB_QUANTRALIB $DEB_QGRAPHER_20_04 $DEB_CHIP_CONVERTER_UBUNTU $DEB_COLANDER"
                 elif [ "$VERSION_CODENAME" == "jammy" ]; then
+                    if [ "$ARCH" != "x86_64" ] && [ "$ARCH" != "armv7l" ]; then
+                        echo "Not supported arch: $ARCH"
+                        exit 1
+                    fi
                     PKGS="$DEB_QUANTRALIB $DEB_QGRAPHER_22_04 $DEB_CHIP_CONVERTER_UBUNTU $DEB_COLANDER"
                 fi
             else
